f6c4f3afd7425b8cf5097392092b12926afa706b
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2018-12-24 15:06:55 +0100

    Add Some Fields for test

--- app/Http/Controllers/Admin/AthleteCrudController.php
+++ app/Http/Controllers/Admin/AthleteCrudController.php
@@ -15,7 +15,54 @@ class AthleteCrudController extends CrudController
 
         /******BASIC CRUD INFORMATIONS*****/
 
+        $this->crud->setModel("App\Models\Athlete");
+        $this->crud->setRoute(config('backpack.base.route_prefix', 'admin') . '/athlete');
+        $this->crud->setEntityNameStrings('athlete', 'athletes');
+
         /******COLUMNS AND FIELDS*****/
 
+        /****COLUMNS****/
+        $this->crud->addColumn([
+            'name' => 'firstname',
+            'label' => 'Prénom'
+        ]);
+
+        $this->crud->addColumn([
+            'name' => 'lastname',
+            'label' => 'Nom'
+        ]);
+
+        /*****FIELDS*****/
+        $this->crud->addField([
+            'name' => 'firstname',
+            'label' => 'Prénom',
+            'type' => 'text',
+            'placeholder' => 'Entrez votre prénom'
+        ]);
+
+        $this->crud->addField([
+            'name' => 'lastname',
+            'label' => 'Nom',
+            'type' => 'text',
+            'placeholder' => 'Entrez votre nom'
+        ]);
+
+        $this->crud->addField([
+            'name' => 'slug',
+            'label' => "Slug (URL)",
+            'type' => 'text',
+            'hint' => 'Est automatiquement généré à partir du nom-prénom si pas remplit.'
+        ]);
+
+    }
+
+    public function store(StoreRequest $request)
+    {
+        return parent::storeCrud();
+    }
+
+    public function update(UpdateRequest $request)
+    {
+        return parent::updateCrud();
     }
 }
